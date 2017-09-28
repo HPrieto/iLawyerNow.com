2b5434c792d40fcc2ce1ae7807b226a6c6e91651
Heriberto Prieto <duskayame@gmail.com>
2017-09-28 11:49:09 -0700

    navbar begun

---
+++ ilawyernow/src/components/navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './styles/navbar.css';
+
+class Navbar extends React.Component {
+	render() {
+		return (
+			<div className="navbar navbar-fixed-top ">
+				<div className="container">
+			        <div className="navbar-header">
+			          	<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				            <span className="sr-only">Toggle navigation</span>
+				            <span className="icon-bar"></span>
+				            <span className="icon-bar"></span>
+				            <span className="icon-bar"></span>
+			          	</button>
+			          	<a className="navbar-brand" href="#">ILAWYER</a>
+			        </div>
+			        <div id="navbar" className="navbar-collapse collapse">
+			        	<div className="navbar-right">
+			        		<button className="navbtn btn btn-default">HELP</button>
+			            	<button className="navbtn btn btn-default">SIGN IN</button>
+			            	<button type="submit" className="btn btn-lawyer" id="">BECOME AN ILAWYER</button>
+			        	</div>
+			        </div>
+	      		</div>
+			</div>
+		);
+	}
+}
+export default Navbar;
