9409fe721bdfeeb159159f5723ad4591f1ea61a8
Heriberto Prieto <duskayame@gmail.com>
2017-10-09 13:45:23 -0700

    file-loader installed

--- webpack.config.js
+++ webpack.config.js
@@ -25,12 +25,13 @@ module.exports = {
         		use: [ 'style-loader', 'css-loader' ]
 			},
 			{
-		        test: /\.(jpe?g|png|gif|svg)$/i,
-		        loaders: [
-		            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-		            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-		        ]
-    		}
+				test: /.*\.(gif|png|jpe?g)$/i,
+  				use: [
+    				{
+      					loader: 'image-webpack-loader'
+    				}
+  				]
+			}
 		]
 	}
 }
