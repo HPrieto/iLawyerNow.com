0d58a0ca86ceb43d1b5e101f3b0510a156158b6a
Heriberto Prieto <duskayame@gmail.com>
2017-10-17 00:55:52 -0700

    navbar-collapse design

--- src/component/containers/Navbar.js
+++ src/component/containers/Navbar.js
@@ -12,16 +12,21 @@ class Navbar extends React.Component {
 		                    <span className="menu-global menu-middle" />
 		                    <span className="menu-global menu-bottom" />
                 		</div>
-                		<a className='navbar-brand home-navbar-brand'>iLAWYER</a>
+                		<a className='navbar-brand home-navbar-brand'>THE LAW CLUB</a>
 					</div>
-					<div className='collapse navbar-collapse' id='home-navbar-collapse'>
-						<ul className='nav navbar-nav zero-padding'>
-							<li><button className='home-navbar-btn home-navbar-btn-left'>Member</button></li>
-							<li><button className='home-navbar-btn home-navbar-btn-left'>Attorney</button></li>
+					<div className='collapse navbar-collapse zero-padding zero-margin' id='home-navbar-collapse'>
+						<ul className='nav navbar-nav zero-padding zero-margin'>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-left home-navbar-btn-bottom-border home-navbar-collapse-top home-navbar-collapse-bottom home-navbar-collapse-btn'>Home</button></li>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-left home-navbar-collapse-top home-navbar-collapse-btn'>Download the App</button></li>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-left home-navbar-uncollapse-btn'>Member</button></li>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-left home-navbar-uncollapse-btn'>Attorney</button></li>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-left home-navbar-collapse-btn'>Sign Up</button></li>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-left home-navbar-collapse-bottom home-navbar-collapse-btn'>Log In</button></li>
 						</ul>
-						<ul className='nav navbar-nav navbar-right zero-padding'>
-							<li><button className='home-navbar-btn home-navbar-btn-right'>HELP</button></li>
-							<li><button className='home-navbar-btn-green'>BECOME A MEMBER</button></li>
+						<ul className='nav navbar-nav navbar-right zero-padding zero-margin'>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-right home-navbar-btn-top-border home-navbar-collapse-top home-navbar-uncollapse-btn'>HELP</button></li>
+							<li className='home-navbar-li'><button className='home-navbar-btn home-navbar-btn-right home-navbar-btn-top-border home-navbar-collapse-top home-navbar-collapse-btn'>Help</button></li>
+							<li className='home-navbar-li'><button className='home-navbar-btn-green home-navbar-uncollapse-btn'>BECOME A MEMBER</button></li>
 						</ul>
 					</div>
 				</div>
