f72e60116d50d0e94b163a47a2831dbcfa22ff0a
Heriberto Prieto <duskayame@gmail.com>
2017-10-20 15:19:46 -0700

    sign-image container added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import BinaryDirectory from './component/layout/BinaryDirectory.js';
 
 class App extends Component {
 	render() {
+
+		/*
+			<BinaryDirectory header={"Sign In"} titles={["Attorney","Member"]}
+						descriptions={["Find everything you need to track your success on the road.","Manage your payment options, review trip history, and more."]}
+						buttonNames={["ATTORNEY SIGN IN","MEMBER SIGN IN"]}/>
+		*/
+
 		return (
 			<div style={{padding: 0, margin: 0}}>
 				<Navbar />
--- src/component/layout/BinaryDirectory.js
+++ src/component/layout/BinaryDirectory.js
@@ -4,9 +4,6 @@ import './layout.css';
 import Footer from '../presentation/Footer.js';
 
 class BinaryDirectory extends Component {
-	componentWillMount() {
-		console.log('Binary Directory')
-	}
 	render() {
 		let tanColor = '#F7F7F8';
 		return (
@@ -17,16 +14,16 @@ class BinaryDirectory extends Component {
 							<h1>{this.props.header}</h1>
 							<div className='white-left-sign-container-body-bottom zero-margin zero-padding'>
 								<div className='col-xs-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 zero-padding zero-margin'>
-									<h3 className='hide-xs'>{this.props.titles[0]}</h3>
-									<p className='hide-xs'>{this.props.descriptions[0]}</p>
+									<h3 className='hide-in-mobile'>{this.props.titles[0]}</h3>
+									<h4 className='transparent-on-mobile'>{this.props.descriptions[0]}</h4>
 									<button className='btn white-left-sign-container-body-btn'>
 										<div className='col-xs-11 col-sm-11 col-md-11 col-lg-11 col-xl-11 zero-padding zero-margin'>{this.props.buttonNames[0]}</div>
 										<div className='col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 zero-padding zero-margin'><span className="glyphicon glyphicon-chevron-right"/></div>
 									</button>
 								</div>
 								<div className='col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 zero-padding zero-margin'>
-									<h3 className='hide-xs'>{this.props.titles[1]}</h3>
-									<p className='hide-xs'>{this.props.descriptions[1]}</p>
+									<h3 className='hide-in-mobile'>{this.props.titles[1]}</h3>
+									<h4 className='transparent-on-mobile'>{this.props.descriptions[1]}</h4>
 									<button className='btn white-left-sign-container-body-btn white-left-sign-container-body-btn-inverse'>
 										<div className='col-xs-11 col-sm-11 col-md-11 col-lg-11 col-xl-11 zero-padding zero-margin'>{this.props.buttonNames[1]}</div>
 										<div className='col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 zero-padding zero-margin'><span className="glyphicon glyphicon-chevron-right"/></div>
@@ -34,6 +31,10 @@ class BinaryDirectory extends Component {
 								</div>
 							</div>
 						</div>
+					</div>
+				</div>
+				<div className="row" style={{backgroundColor: tanColor}}>
+					<div className='container row-container image-row-container'>
 					</div>
 				</div>
 				<Footer buttonNames={['LAWYER SIGN UP', 'MEMBER SIGN UP']} buttonIds={['LawyerSignup', 'MemberSignup']} />
