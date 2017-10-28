3800946100b4bb1c01c3fa650944cef44c0c503d
Heriberto Prieto <duskayame@gmail.com>
2017-10-28 02:38:31 -0700

    signup form created

--- src/component/layout/Signup.js
+++ src/component/layout/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './layout.css';
 
 /* Import Components */
@@ -6,11 +7,37 @@ import SignupForm from '../containers/SignupForm.js';
 
 class Signup extends Component {
 	render() {
+		let tanColor = '#F7F7F8';
 		return (
-			<div className='container-fluid signup-container'>
-				<div className='signup-body'>
-					<h1>Become a Law Club Member</h1>
-					<SignupForm />
+			<div className='container main-container signup-container'>
+				<div className="row" style={{backgroundColor: 'white'}}>
+					<div className='container row-container signup-header-section'>
+						<div className='col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10'>
+							<h1>Ride with Uber</h1>
+							<h2>Earn money on your schedule</h2>
+						</div>
+						<div className='col-xs-0 col-sm-0 col-md-2 col-lg-2 col-xl-2'></div>
+					</div>
+				</div>
+				<div className="row" style={{backgroundColor: 'black'}}>
+					<div className='container row-container'>
+						<img className='signup-image-section'/>
+					</div>
+				</div>
+				<div className="row" style={{backgroundColor: 'white', height: '500px'}}>
+					<div className='container row-container'>
+						
+					</div>
+				</div>
+				<div className="row" style={{backgroundColor: tanColor, height: '500px'}}>
+					<div className='container row-container'>
+						
+					</div>
+				</div>
+				<div className="row" style={{backgroundColor: 'white', height: '500px'}}>
+					<div className='container row-container'>
+						
+					</div>
 				</div>
 			</div>
 		)
